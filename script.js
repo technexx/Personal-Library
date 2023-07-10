0c7db0119ce3d406f7c41ab673906135c9b5da49
Brian Salomone <brian.j.salomone@gmail.com>
2023-07-10 11:56:03 -0700

    add method directly to dom

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+//Todo: (a)refresh DOM from array each time list is modified or (b)insert elements directly and keep mirror array or (c)insert elements directly and if needed populate an array by querying them.
+
 const bookCards = document.querySelector(".card-holder")
 const titleField = document.getElementById("title-form")
 const authorField = document.getElementById("author-form")
@@ -6,36 +8,59 @@ const pagesField = document.getElementById("pages-form")
 const hadReadCheckbox = document.getElementById("read-checkbox")
 const submitButton = document.getElementById("submit")
 
-function populateBooks(bookList) {
-    for (let i=0; i<Object.keys(bookList).length; i++) {
-        const bookDiv = document.createElement("div")
-        bookDiv.classList.add("book")
-
-        const titleDiv = document.createElement("div")
-        const authorDiv = document.createElement("div")
-        const pagesDiv = document.createElement("div")
-        const hasReadDiv = document.createElement("div")
-
-        titleDiv.classList.add("title")
-        authorDiv.classList.add("author")
-        pagesDiv.classList.add("pages")
-        hasReadDiv.classList.add("hasRead")
-
-        const hasReadButton = document.createElement("button")
-        hasReadButton.innerText = bookList[i].hasRead
-        hasReadDiv.appendChild(hasReadButton)
-
-        titleDiv.innerText = "\"" + bookList[i].title + "\""
-        authorDiv.innerText = bookList[i].author
-        pagesDiv.innerText = bookList[i].pages + " pages"
-
-        bookDiv.appendChild(titleDiv)
-        bookDiv.appendChild(authorDiv)
-        bookDiv.appendChild(pagesDiv)
-        bookDiv.appendChild(hasReadDiv)
+submitButton.addEventListener("click", () => {
     
-        bookCards.appendChild(bookDiv)
+    if (checkFormFieldsHaveText()) {
+        addBookToLibrary(bookObjectFromForm())
+        dismissPopup()
     }
+})
+
+function checkFormFieldsHaveText() {
+    // console.log(titleField.value)
+    // console.log(authorField.value)
+    // console.log(pagesField.value)
+
+    if (titleField.value.length >=2 && authorField.value.length >=2 && pagesField.value.length >= 1) { return true; }
+}
+
+function dismissPopup() { window.location = "#" }
+
+function addBookToLibrary(bookObject) {
+    console.log("book object is " + bookObject)
+    const bookDiv = document.createElement("div")
+    bookDiv.classList.add("book")
+
+    const titleDiv = document.createElement("div")
+    const authorDiv = document.createElement("div")
+    const pagesDiv = document.createElement("div")
+    const hasReadDiv = document.createElement("div")
+
+    titleDiv.classList.add("title")
+    authorDiv.classList.add("author")
+    pagesDiv.classList.add("pages")
+    hasReadDiv.classList.add("hasRead")
+    
+    titleDiv.innerText = bookObject.title
+    authorDiv.innerText = bookObject.author
+    pagesDiv.innerText = bookObject.pages + " pages"
+
+    const hasReadButton = document.createElement("button")
+    hasReadButton.innerText = bookObject.hasRead
+    hasReadDiv.appendChild(hasReadButton)
+
+    bookDiv.appendChild(titleDiv)
+    bookDiv.appendChild(authorDiv)
+    bookDiv.appendChild(pagesDiv)
+    bookDiv.appendChild(hasReadDiv)
+
+    bookCards.appendChild(bookDiv)
+}
+
+function bookObjectFromForm() {
+    return new Book (
+        titleField.value, authorField.value, pagesField.value, hadReadCheckbox.value
+    )
 }
 
 function Book(title, author, pages, hasRead) {
@@ -54,27 +79,8 @@ function Book(title, author, pages, hasRead) {
     }
 }
 
+///////////////////////////////////////////////////////////
+
 let myLibrary = [new Book("A Tale of Two Tattle Tales", "Zac Caz", "666", true), new Book("No Dogs Go To Hell", "Miff Stabson", "18", true),  new Book("How Can It Be When It Ain't So?", "Railyard Chechnya", "1502", false)]
 
-submitButton.addEventListener("click", () => {
-    if (checkFormFieldsHaveText()) {
-        addBookToLibrary()
-        populateBooks(myLibrary)
-        dismissPopup()
-    }
-})
-
-function checkFormFieldsHaveText() {
-    if (titleField.value.length >=2 && authorField.value.length >=2 && pagesField.value.length >= 1) { return true }
-}
-
-function addBookToLibrary() {
-    let book = new Book (
-        titleField.value, authorField.value, pagesField.value, hadReadCheckbox.value
-    )
-    myLibrary.push(book)
-}
-
-function dismissPopup() { window.location = "#" }
-
-populateBooks(myLibrary)
+// populateBooks(myLibrary)
